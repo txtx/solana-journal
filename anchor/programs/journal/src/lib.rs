6443b12ef1b5ec0756f6c4433576a380c2d51556
MicaiahReid <micaiahreid@gmail.com>
2025-06-18 14:30:36 -0400

    add cpi events to program

--- anchor/programs/journal/src/lib.rs
+++ anchor/programs/journal/src/lib.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 // This is your program's public key and it will update
 // automatically when you build the project.
-declare_id!("8sddtWW1q7fwzspAfZj4zNpeQjpvmD3EeCCEfnc3JnuP");
+declare_id!("EZB64BQPMPzGNEV6XvrxTSPcQHCXaRF7aXMgunxQ6LNh");
 
 #[program]
 mod journal {
@@ -18,6 +18,12 @@ mod journal {
         msg!("Message: {}", message);
 
         let journal_entry = &mut ctx.accounts.journal_entry;
+        emit_cpi!(JournalEntryCreated::new(
+            ctx.accounts.owner.key(),
+            &title,
+            &message,
+            journal_entry.key()
+        ));
         journal_entry.owner = ctx.accounts.owner.key();
         journal_entry.title = title;
         journal_entry.message = message;
@@ -33,14 +39,25 @@ mod journal {
         msg!("Title: {}", title);
         msg!("Message: {}", message);
 
+
+        emit_cpi!(JournalEntryUpdated::new(
+            ctx.accounts.owner.key(),
+            &message,
+            ctx.accounts.journal_entry.key()
+        ));
+
         let journal_entry = &mut ctx.accounts.journal_entry;
         journal_entry.message = message;
 
         Ok(())
     }
 
-    pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, title: String) -> Result<()> {
+    pub fn delete_journal_entry(ctx: Context<DeleteEntry>, title: String) -> Result<()> {
         msg!("Journal entry titled {} deleted", title);
+        emit_cpi!(JournalEntryDeleted::new(
+            ctx.accounts.owner.key(),
+            ctx.accounts.journal_entry.key()
+        ));
         Ok(())
     }
 }
@@ -54,6 +71,7 @@ pub struct JournalEntryState {
 
 #[derive(Accounts)]
 #[instruction(title: String, message: String)]
+#[event_cpi]
 pub struct CreateEntry<'info> {
     #[account(
         init,
@@ -70,6 +88,7 @@ pub struct CreateEntry<'info> {
 
 #[derive(Accounts)]
 #[instruction(title: String, message: String)]
+#[event_cpi]
 pub struct UpdateEntry<'info> {
     #[account(
         mut,
@@ -87,6 +106,7 @@ pub struct UpdateEntry<'info> {
 
 #[derive(Accounts)]
 #[instruction(title: String)]
+#[event_cpi]
 pub struct DeleteEntry<'info> {
     #[account( 
         mut, 
@@ -98,4 +118,67 @@ pub struct DeleteEntry<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[event]
+/// Represents a journal entry creation event.
+pub struct JournalEntryCreated {
+    /// The public key of the owner of the journal entry.
+    pub owner: Pubkey,
+    /// The title of the journal entry.
+    pub title: String,
+    /// The content of the journal entry.
+    pub message: String,
+    /// The unique identifier for the journal entry.
+    pub entry_id: Pubkey,
+}
+
+impl JournalEntryCreated {
+    pub fn new(owner: Pubkey, title: &str, message: &str, entry_id: Pubkey) -> Self {
+        Self {
+            owner,
+            title: title.to_string(),
+            message: message.to_string(),
+            entry_id,
+        }
+    }
+}
+
+#[event]
+/// Represents a journal entry update event.
+pub struct JournalEntryUpdated {
+    /// The public key of the owner of the journal entry.
+    pub owner: Pubkey,
+    /// The updated message of the journal entry.
+    pub message: String,
+    /// The unique identifier for the journal entry.
+    pub entry_id: Pubkey,
+}
+
+impl JournalEntryUpdated {
+    pub fn new(owner: Pubkey, message: &str, entry_id: Pubkey) -> Self {
+        Self {
+            owner,
+            message: message.to_string(),
+            entry_id,
+        }
+    }
+}
+
+#[event]
+/// Represents a journal entry deletion event.
+pub struct JournalEntryDeleted {
+    /// The public key of the owner of the journal entry.
+    pub owner: Pubkey,
+    /// The unique identifier for the journal entry.
+    pub entry_id: Pubkey,
+}
+
+impl JournalEntryDeleted {
+    pub fn new(owner: Pubkey, entry_id: Pubkey) -> Self {
+        Self {
+            owner,
+            entry_id,
+        }
+    }
 }
